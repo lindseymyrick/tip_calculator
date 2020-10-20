b44b73b679d1158c99dfd25b7b2efbf8f756eed2
Lindsey <lindsey@blacktopinteractive.com>
2020-10-20 09:36:48 -0500

    tip buttons selecting and deselecting

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Lindsey Myrick on 10/20/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet var totalLabel: UILabel!
+    
+    @IBOutlet var settingLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
